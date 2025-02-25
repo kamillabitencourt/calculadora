d8a31c17522e86c1159f14dc41af47407110f064
Kamilla <kamillabitencourt18@gmail.com>
2025-02-25 13:07:54 -0300

    primeira versao

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+function appendValue(value) {
+    document.getElementById('display').value += value;
+}
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch {
+        document.getElementById('display').value = 'Erro';
+    }
+}
